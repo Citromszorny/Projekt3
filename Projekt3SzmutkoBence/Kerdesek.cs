6ddbfb7db603eae0e1cfcfce4eace5882dbd6b42
Citromszorny <bence5605@gmail.com>
2022-02-27 19:32:41 +0100

    Elkezdeni meg próbálkozni

---
+++ Projekt3SzmutkoBence/Kerdesek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt3SzmutkoBence
+{
+    class Kerdes
+    {
+        public string temakor { get; }
+        public string kerdes { get; }
+        public int valasz { get;}
+        public List<string> valaszok { get; }
+
+        public Kerdes(string temakor, string kerdes, List<string> valaszok, int valasz) 
+        {
+            this.temakor = temakor;
+            this.kerdes = kerdes;
+            this.valaszok = valaszok;
+            this.valasz = valasz;
+        }
+    }
+    
+}
---
+++ Projekt3SzmutkoBence/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Projekt3SzmutkoBence
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Kerdes> Kerdesek = new List<Kerdes>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] sorok = File.ReadAllLines("torikerdes.txt");
+            foreach (string sor in sorok)
+            {
+                string[] adatok = sor.Split(new char[] { ';' });
+                string temakor = adatok[0];
+                string kerdes = adatok[1];
+                int helyesValasz = int.Parse(adatok[2]);
+                List<string> valaszok = new List<string>();
+                for (int i = 3; i < adatok.Length; i++)
+                {
+                    valaszok.Add(adatok[i]);
+                }
+                Kerdesek.Add(new Kerdes(temakor, kerdes, valaszok, helyesValasz));
+            }
+
+            temaValasztas.Items.Add("Történelem");
+            temaValasztas.Items.Add("Matematika");
+            temaValasztas.Items.Add("Irodalom");
+            
+
+        }
+
+        private void megerosites_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1();
+            win2.Show();
+            this.Close();
+        }
+
+        private void temaValasztas_Selected(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
