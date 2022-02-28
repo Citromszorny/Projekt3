22861f838783516268a234d006f2c442a5fc3f4c
Citromszorny <bence5605@gmail.com>
2022-02-28 17:28:02 +0100

    Random sorrendben kérdésbetöltés, kiválasztott témának megfelelően, kiértékelés

--- Projekt3SzmutkoBence/Window1.xaml.cs
+++ Projekt3SzmutkoBence/Window1.xaml.cs
@@ -22,46 +22,65 @@ namespace Projekt3SzmutkoBence
     {
         List<Kerdes> Kerdesek;
         Kerdes jelenlegiKerdes;
+        Random rnd = new Random();
+        List<int> lista = new List<int>();
+        int osszes = 0;
+        int eltalalt = 0;
         public Window1(List<Kerdes> Kerdesek, string tema)
         {
             InitializeComponent();
-            Random rnd = new Random();
-            List<int> lista = new List<int>();
+            
+            
+            
+            this.Kerdesek = Kerdesek;
+            BetoltUjKerdes(Kerdesek, rnd, lista);
+
+        }
+        private void visszaSzinez() 
+        {
+            valasz1.Background = Brushes.White;
+            valasz2.Background = Brushes.White;
+            valasz3.Background = Brushes.White;
+            valasz4.Background = Brushes.White;
+        }
+        private void BetoltUjKerdes(List<Kerdes> Kerdesek, Random rnd, List<int> lista)
+        {
+            lista.Clear();
             while (lista.Count < 4)
             {
                 int szam = rnd.Next(0, 4);
                 if (lista.Contains(szam))
                 {
-                    
+
                 }
                 else
                 {
                     lista.Add(szam);
                 }
-                
             }
-            this.Kerdesek = Kerdesek;
-            jelenlegiKerdes = Kerdesek[rnd.Next(0,Kerdesek.Count)];
+            visszaSzinez();
+            jelenlegiKerdes = Kerdesek[rnd.Next(0, Kerdesek.Count)];
             KerdesLabel.Content = jelenlegiKerdes.kerdes;
             valasz1.Content = jelenlegiKerdes.valaszok[lista[0]];
             valasz2.Content = jelenlegiKerdes.valaszok[lista[1]];
             valasz3.Content = jelenlegiKerdes.valaszok[lista[2]];
             valasz4.Content = jelenlegiKerdes.valaszok[lista[3]];
-
+            osszes++;
+            
         }
-
-
 
         private void valasz1_Click(object sender, RoutedEventArgs e)
         {
             if (valasz1.Content == jelenlegiKerdes.helyesValasz)
             {
                 valasz1.Background = Brushes.Green;
+                eltalalt++;
             }
             else
             {
                 valasz1.Background = Brushes.Red;
             }
+            
         }
 
         private void valasz2_Click(object sender, RoutedEventArgs e)
@@ -69,11 +88,13 @@ namespace Projekt3SzmutkoBence
             if (valasz2.Content == jelenlegiKerdes.helyesValasz)
             {
                 valasz2.Background = Brushes.Green;
+                eltalalt++;
             }
             else
             {
                 valasz2.Background = Brushes.Red;
             }
+            
         }
 
         private void valasz3_Click(object sender, RoutedEventArgs e)
@@ -81,11 +102,13 @@ namespace Projekt3SzmutkoBence
             if (valasz3.Content == jelenlegiKerdes.helyesValasz)
             {
                 valasz3.Background = Brushes.Green;
+                eltalalt++;
             }
             else
             {
                 valasz3.Background = Brushes.Red;
             }
+            
         }
 
         private void valasz4_Click(object sender, RoutedEventArgs e)
@@ -93,11 +116,23 @@ namespace Projekt3SzmutkoBence
             if (valasz4.Content == jelenlegiKerdes.helyesValasz)
             {
                 valasz4.Background = Brushes.Green;
+                eltalalt++;
             }
             else
             {
                 valasz4.Background = Brushes.Red;
             }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BetoltUjKerdes(Kerdesek, rnd, lista);
+        }
+
+        private void kiertekeles_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Eredmény: {osszes}/{eltalalt}", "Kiértékelés");
         }
     }
 }
